a458e9481415c7ee46289350730f47249b8952b1
AgentUnicorn <superslap1999@gmail.com>
2021-12-27 19:11:38 +0700

    fix render more icon when switch account

--- src/Components/AccountInfo.js
+++ src/Components/AccountInfo.js
@@ -11,7 +11,7 @@ export default function AccountInfo({userBalance, defaultAccount}) {
             if(defaultAccount !== null) {
                 const jazz = parseInt(defaultAccount.slice(2,10),16);
                 const newJazz = Jazzicon(16,jazz);
-                console.log(newJazz);
+                appendRef.current.removeChild(appendRef.current.lastChild);
                 appendRef.current.appendChild(newJazz);
             }
         }
@@ -23,6 +23,7 @@ export default function AccountInfo({userBalance, defaultAccount}) {
                 <span className='px-2'>{userBalance} BNB</span>
                 <div ref={appendRef} className='px-2 py-1 bg-black  rounded-lg flex items-center'>
                     <span className={defaultAccount !== null?`mr-2`:''}>{defaultAccount !== null?`${defaultAccount.slice(0,6)}...${defaultAccount.slice(defaultAccount.length-4,defaultAccount.length)}`:'No acco'}</span>
+                    <div></div>
                 </div>
             </div>
         </div>
